8cf492b4bfde0ec6d6727d05a427595a2f660e11
Ben Andersen <bandersen@delkorsystems.com>
2017-11-13 15:27:22 -0600

    Update DAO to saveOrUpdate
    saveOrUpdate needed for shared DAO function

--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -44,7 +44,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 		
 		
 		// save the customer to the database
-		currentSession.save(theCustomer);
+		currentSession.saveOrUpdate(theCustomer);
 		
 	}
 
